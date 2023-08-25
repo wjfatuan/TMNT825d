cbc6ba8b03e1fd4f4546aa1885c01bdaadd21503
wjfatuan <wilson.forero@uan.edu.co>
2023-08-25 18:48:09 -0500

    Agregar un boton y capturar el evento de clic

--- app/src/main/java/co/edu/uan/android/tmnt825d/MainActivity.kt
+++ app/src/main/java/co/edu/uan/android/tmnt825d/MainActivity.kt
@@ -2,10 +2,17 @@ package co.edu.uan.android.tmnt825d
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main) // cargar el archivo de layout de la actividad
+
+        val btn = findViewById<Button>(R.id.btnHello)
+        btn.text = "Boton Hola"
+        btn.setOnClickListener {
+            println("Clic en el boton hola")
+        }
     }
 }
