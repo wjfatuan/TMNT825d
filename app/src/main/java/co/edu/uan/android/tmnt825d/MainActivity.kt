cc27ddd6367e91dfa89064d565e7603ce05682f7
wjfatuan <wilson.forero@uan.edu.co>
2023-08-25 18:35:10 -0500

    Initial commit

---
+++ app/src/main/java/co/edu/uan/android/tmnt825d/MainActivity.kt
@@ -0,0 +1,11 @@
+package co.edu.uan.android.tmnt825d
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main) // cargar el archivo de layout de la actividad
+    }
+}
