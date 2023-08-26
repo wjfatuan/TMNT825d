d9f7ca109e59417e06c9945192df85ddc21faa50
wjfatuan <wilson.forero@uan.edu.co>
2023-08-25 19:35:02 -0500

    Uso del ImageView y manejo de eventos

--- app/src/main/java/co/edu/uan/android/tmnt825d/MainActivity.kt
+++ app/src/main/java/co/edu/uan/android/tmnt825d/MainActivity.kt
@@ -2,19 +2,29 @@ package co.edu.uan.android.tmnt825d
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import co.edu.uan.android.tmnt825d.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater) // creando el binding de la actividad
+        binding = ActivityMainBinding.inflate(layoutInflater) // creando el binding de la actividad
         setContentView(binding.root) // usar el layout contenido en el binding
-
-        //val btn = findViewById<Button>(R.id.btnHello) -- no se necesita con el binding
-        binding.btnHello.text = "Boton Hola"
-        binding.btnHello.setOnClickListener {
-            println("Clic en el boton hola")
+        binding.btnMikey.setOnClickListener {
+            showTurtle(R.drawable.mikey)
         }
+        binding.btnRaf.setOnClickListener {
+            showTurtle(R.drawable.raf)
+        }
+        binding.btnLeo.setOnClickListener {
+            showTurtle(R.drawable.leo)
+        }
+        binding.btnDonatello.setOnClickListener {
+            showTurtle(R.drawable.donatello)
+        }
+    }
+
+    fun showTurtle(img: Int) {
+        binding.imgTurtle.setImageResource(img)
     }
 }
