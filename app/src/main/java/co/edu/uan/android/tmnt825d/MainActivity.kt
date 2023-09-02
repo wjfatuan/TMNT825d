34edd21b042cb3b395484162d9f0c95d40e12c35
wjfatuan <wilson.forero@uan.edu.co>
2023-09-01 19:19:45 -0500

    Uso del ListView y los adapter

--- app/src/main/java/co/edu/uan/android/tmnt825d/MainActivity.kt
+++ app/src/main/java/co/edu/uan/android/tmnt825d/MainActivity.kt
@@ -2,10 +2,13 @@ package co.edu.uan.android.tmnt825d
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.TextView
 import co.edu.uan.android.tmnt825d.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+    val turtleNames = arrayListOf<String>("leo", "raf")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater) // creando el binding de la actividad
@@ -22,6 +25,28 @@ class MainActivity : AppCompatActivity() {
         binding.btnDonatello.setOnClickListener {
             showTurtle("donatello")
         }
+        binding.radioMikey.setOnClickListener {
+            showTurtle("mikey")
+        }
+        binding.radioRaf.setOnClickListener {
+            showTurtle("raf")
+        }
+        binding.radioLeo.setOnClickListener {
+            showTurtle("leo")
+        }
+        binding.radioDonatello.setOnClickListener {
+            showTurtle("donatello")
+        }
+        // 1: actividad, 2: layout con el q se mostrararn los datos, 3: los datos
+        val myAdapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, turtleNames)
+        binding.lvTurtles.adapter = myAdapter
+
+        binding.lvTurtles.setOnItemClickListener { parent, view, position, id ->
+            println("parent: $parent \n view: $view \n position: $position \n id: $id")
+            val tv = view as TextView
+            showTurtle(tv.text.toString().lowercase())
+        }
+
     }
 
     fun showTurtle(name: String) {
@@ -29,5 +54,6 @@ class MainActivity : AppCompatActivity() {
         binding.imgTurtle.setImageResource(imgId)
         val textId = resources.getIdentifier("turtle_$name", "string", packageName)
         binding.txtDescription.text = resources.getText(textId).toString()
+        binding.radioLeo.isChecked = true
     }
 }
